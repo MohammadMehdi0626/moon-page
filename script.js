ef8ff42628d8e4ce0ee7d1fa0be62dc60cf2c9af
MohammadMehdi0626 <mehdiebi0626@gmail.com>
2026-08-29 12:48:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 document.addEventListener(
     "DOMContentLoaded",
     function () {
-
+        alert("SCRIPT RUNNING");
         // =====================================================
         // SESSION
         // =====================================================
